3ae9f2b79e9f1b2cfd63db0a9999eefda007b1a1
senhai <95114113+senhai-kun@users.noreply.github.com>
2022-12-14 08:24:58 +0800

    Update baseUrl.js

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,8 +1,8 @@
 
 let baseUrl = "http://localhost:5000";
 
-if ( window.location.origin === "https://find-mentor.vercel.app" || window.location.origin === "https://mentorverse.vercel.app" ) {
-    baseUrl = "https://find-a-mentor-server.vercel.app";
+if ( window.location.origin === "https://mentorverse.vercel.app" ) {
+    baseUrl = "https://mentorverse.vercel.app";
 } else {
     baseUrl = "http://localhost:5000";
 }
